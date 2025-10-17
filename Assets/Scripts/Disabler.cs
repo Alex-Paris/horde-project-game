f81637e7f5af23575f3c950e026a6595aa1a853f
Alex Paris <alex_paris28@hotmail.com>
2025-10-17 01:25:51 -0300

    chore: horde project final

---
+++ Assets/Scripts/Disabler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disabler : MonoBehaviour
+{
+	public EnemyPooling leftEnemyPool;
+	public EnemyPooling rightEnemyPool;
+	
+	private void OnDisable()
+	{
+		leftEnemyPool.DestroyAllEnemies();
+		rightEnemyPool.DestroyAllEnemies();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		collision.attachedRigidbody.gameObject.SetActive(false);
+	}
+}
